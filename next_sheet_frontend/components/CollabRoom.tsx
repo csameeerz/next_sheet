1dd6dc780e25982c1e885e814c7e803a566deb7f
Sameer Choudhary <csameeerz.08@gmail.com>
2024-08-03 22:38:01 +0530

    added homepage with no documents ui

--- next_sheet_frontend/components/CollabRoom.tsx
+++ next_sheet_frontend/components/CollabRoom.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import React from 'react'
 import { RoomProvider, ClientSideSuspense } from "@liveblocks/react/suspense";
 import { Editor } from '@/components/editor/Editor';
--- next_sheet_frontend/components/Header.tsx
+++ next_sheet_frontend/components/Header.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
+import { cn } from '@/lib/utils';
 
-const Header = ({ children }: HeaderProps) => {
+const Header = ({ children, className }: HeaderProps) => {
   return (
-    <div className='header'>
+    <div className={cn('header', className)}>
       <Link href='/' className="md:flex-1">
         <Image src="/assets/icons/logo.svg" alt="Next Sheet" width={120} height={32} className="hidden md:block"/>
         <Image src="/assets/icons/logo-icon.svg" alt="Next Sheet Logo" width={32} height={32} className="mr-2 md:hidden"/>
