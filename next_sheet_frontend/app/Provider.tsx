0b7d4819b3040f77fba1f592aabdc85a5178aee7
Sameer Choudhary <csameeerz.08@gmail.com>
2024-08-04 13:11:35 +0530

    added comments' mentions by rooms

--- next_sheet_frontend/app/Provider.tsx
+++ next_sheet_frontend/app/Provider.tsx
@@ -3,15 +3,28 @@
 import { ReactNode } from "react";
 import { LiveblocksProvider, ClientSideSuspense } from "@liveblocks/react/suspense";
 import Loader from "@/components/Loader";
-import { getClerkUsers } from "@/lib/actions/users.actions";
+import { getClerkUsers, getDocumentUsers } from "@/lib/actions/users.actions";
+import { currentUser } from "@clerk/nextjs/server";
+import { useUser } from "@clerk/nextjs";
 
 const Provider = ({ children }: { children: ReactNode }) => {
+  const { user: clerkUser } = useUser();
 
   return (
     <LiveblocksProvider authEndpoint="/api/liveblocks-auth" resolveUsers={async ({ userIds }) => {
       const users = await getClerkUsers({ userIds });
       return users;
-    }}>
+    }}
+      resolveMentionSuggestions={async ({text, roomId}) => {
+        const roomUsers = await getDocumentUsers({
+          roomId,
+          currentUser: clerkUser?.emailAddresses[0].emailAddress!,
+          text,
+        })
+
+        return roomUsers;
+      }}
+    >
         <ClientSideSuspense fallback={<Loader />}>
           {children}
         </ClientSideSuspense>
