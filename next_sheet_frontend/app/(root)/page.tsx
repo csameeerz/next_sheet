a2347a85749590f3f0132acc4c4c3556c988c24d
Sameer Choudhary <csameeerz.08@gmail.com>
2024-08-04 10:53:52 +0530

    added existing documents and edit title on homepage

--- next_sheet_frontend/app/(root)/page.tsx
+++ next_sheet_frontend/app/(root)/page.tsx
@@ -4,14 +4,19 @@ import { SignedIn, UserButton } from '@clerk/nextjs';
 import { currentUser } from '@clerk/nextjs/server';
 import Image from 'next/image';
 import { redirect } from 'next/navigation';
+import { getAllDocuments } from '@/lib/actions/room.actions';
+import Link from 'next/link';
+import { DeleteModal } from '@/components/DeleteModal';
+import { dateConverter } from '@/lib/utils';
 
 const Home = async () => {
-  const documents = [];
   const clerkUser = await currentUser();
 
   if(!clerkUser) {
     redirect('/sign-in');
   }
+
+  const roomDocuments = await getAllDocuments(clerkUser.emailAddresses[0].emailAddress);
 
   return (
     <main className='home-container'>
@@ -24,10 +29,29 @@ const Home = async () => {
         </div>
       </Header>
 
-      {documents.length > 0 ? (
-        <div>
-
+      {roomDocuments.data.length > 0 ? (
+        <div className="document-list-container">
+        <div className="document-list-title">
+          <h3 className="text-28-semibold">Existing Documents</h3>
+          <AddDocumentButton userId={clerkUser.id} email={clerkUser.emailAddresses[0].emailAddress} />
         </div>
+        <ul className="document-ul">
+          {roomDocuments.data.map(({ id, metadata, createdAt }: any) => (
+            <li key={id} className="document-list-item">
+              <Link href={`/documents/${id}`} className="flex flex-1 items-center gap-4">
+                <div className="hidden rounded-md bg-dark-500 p-2 sm:block">
+                  <Image src="/assets/icons/doc.svg" alt="file" width={40} height={40} />
+                </div>
+                <div className="space-y-1">
+                  <p className="line-clamp-1 text-lg">{metadata.title}</p>
+                  <p className="text-sm font-light text-blue-100">Created about {dateConverter(createdAt)}</p>
+                </div>
+              </Link>
+              {/* <DeleteModal roomId={id} /> */}
+            </li>
+          ))}
+        </ul>
+      </div>
       ): (
         <div className='document-list-empty'>
           <Image src='/assets/icons/doc.svg' alt='Document' width={40} height={40} className='mx-auto' />
--- next_sheet_frontend/lib/actions/room.actions.ts
+++ next_sheet_frontend/lib/actions/room.actions.ts
@@ -22,7 +22,7 @@ export const createDocument = async ({ userId, email}: CreateDocumentParams) => 
         const room = await liveblocks.createRoom(roomId, {
             metadata,
             usersAccesses,
-            defaultAccesses: []
+            defaultAccesses: ["room:write"]
         })
 
         revalidatePath('/');
@@ -32,3 +32,44 @@ export const createDocument = async ({ userId, email}: CreateDocumentParams) => 
         console.log(`Error in room creation: ${error}`);
     }
 }
+
+export const getDocument = async ({ roomId, userId}: {roomId: string; userId: string}) => {
+    try {
+        const room = await liveblocks.getRoom(roomId);
+        const hasAccess = Object.keys(room.usersAccesses).includes(userId);
+
+        if(!hasAccess) {
+            throw new Error('You do not have the required access to access this document');
+        }
+
+        return parseStringify(room);
+    } catch(error) {
+        console.log(`Error while accessing the room: ${error}`);
+    }
+};
+
+export const getAllDocuments = async (email: string) => {
+    try {
+        const rooms = await liveblocks.getRooms({userId: email});
+
+        return parseStringify(rooms);
+    } catch(error) {
+        console.log(`Error while accessing the rooms: ${error}`);
+    }
+};
+
+export const updateDocument = async (roomId: string, title: string) => {
+    try {
+        const updatedRoom = await liveblocks.updateRoom(roomId, {
+            metadata: {
+                title
+            }
+        });
+        
+        revalidatePath('/document/${roomId}');
+
+        return parseStringify(updatedRoom);
+    } catch(error) {
+        console.log(`Error while updating the room: ${error}`);
+    }
+};
